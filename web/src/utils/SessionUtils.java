0077fa964df868e02f1f4c4271cd1f4546153383
davidmdi <61587193+davidmdi@users.noreply.github.com>
2020-10-24 16:18:33 +0300

    added get functions in session utils : getOrder , getStoreSingleOrderItemsMap

--- web/src/utils/SessionUtils.java
+++ web/src/utils/SessionUtils.java
@@ -3,11 +3,14 @@ package utils;
 
 import constants.Constants;
 import logic.Logic.Engine;
+import logic.Logic.My_CLASS.MyOrder;
+import logic.Logic.My_CLASS.MyStoreSingleOrderItems;
 import logic.Logic.My_CLASS.User;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.Map;
 
 public class SessionUtils {
 
@@ -34,6 +37,18 @@ public class SessionUtils {
         HttpSession session = request.getSession(false);
         Object sessionAttribute = session != null ? session.getAttribute(Constants.SELECTED_ZONE) : null;
         return sessionAttribute != null ? sessionAttribute.toString() : null;
+    }
+
+    public static MyOrder getOrder (HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        MyOrder sessionAttribute = session != null ? (MyOrder)session.getAttribute(Constants.CUSTOMER_ORDER) : null;
+        return sessionAttribute != null ? sessionAttribute : null;
+    }
+    public static Map<Integer, MyStoreSingleOrderItems> getStoreSingleOrderItemsMap(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        Map<Integer, MyStoreSingleOrderItems> sessionAttribute = session != null ?
+            (Map<Integer, MyStoreSingleOrderItems>)session.getAttribute(Constants.CUSTOMER_ORDER) : null;
+        return sessionAttribute != null ? sessionAttribute : null;
     }
     /*
         public static User getUser(HttpServletRequest request, ServletContext servletContext) {
