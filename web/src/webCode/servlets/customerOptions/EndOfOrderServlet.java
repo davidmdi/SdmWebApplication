efc30be4a52b06bda8da72b0582e265cc33552cb
oryom <58296468+oryom@users.noreply.github.com>
2020-10-25 14:27:26 +0200

    Feedbacks working

--- web/src/webCode/servlets/customerOptions/EndOfOrderServlet.java
+++ web/src/webCode/servlets/customerOptions/EndOfOrderServlet.java
@@ -1,23 +1,14 @@
 package webCode.servlets.customerOptions;
 
-import com.google.gson.Gson;
-import logic.Logic.Engine;
-import logic.Logic.My_CLASS.MyFeedback;
-import utils.ServletUtils;
 import utils.SessionUtils;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.text.ParseException;
 
 public class EndOfOrderServlet extends HttpServlet {
-
-    private final String SINGLE_AREA_PAGE_URL = "pages/singleAreaPage/singleArea.html";//"../singleAreaPage/singleArea.html";
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
